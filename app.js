b957d77a5024cbf52a2ac89fe6632a7e6e9f2c4c
Andrew Poksay <andrew@andrews-air.lan>
2017-08-27 13:47:30 -0700

    layer stripes, email collection, google analytics

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function(req,res) {
   res.sendfile('public/index.html');
 });
 
+app.get('/video', function(req,res) {
+  res.sendfile('public/video.html');
+});
+
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000);
